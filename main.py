04b50e1fc36755687096941d5f467613291c0804
afsha <147975801+afshamsayyed@users.noreply.github.com>
2026-02-11 21:04:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder: Afsha Sayyed
+# Date: 30/01/2026
 
 print("--- Factorial Finder ---\n")
+number = int(input())
 
+if number < 0:
+    print(f"Factorial of {abs(number)} is Not Defined")
+else:
+    factorial = 1
+    for i in range(1, number + 1):
+        factorial *= i
+    print(f"Factorial of {number} is {factorial}")
+  
 
-# Write your code here
